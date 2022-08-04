636ea491738a70ba3c2fbc6119b533c9ffa71e4d
shashankgoud002 <101133749+shashankgoud001@users.noreply.github.com>
2022-08-04 09:54:09 +0530

    minorchanges

--- assets/php/suggestion.php
+++ assets/php/suggestion.php
@@ -1,8 +1,17 @@
 <?php 
+        $url = $_SERVER['HTTP_REFERER'];
+
     if(isset($_POST['suggestion'])){
         include_once "connect.php";
         $sql = "INSERT INTO suggestions_data(fname,email,phone,message) VALUES('".$_POST['name']."','".$_POST['email']."','".$_POST['phone']."','".$_POST['message']."')";
         $query = $conn->query($sql);
 
     }
+
+    ob_start();
+                while (ob_get_status()) 
+                {
+                    ob_end_clean();
+                }
+                header( "Location: $url" );
 ?>
